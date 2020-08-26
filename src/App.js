95e55591d5c41b35950127f1d9713529cba329e8
olivercastroteran <olivercastroteran@gmail.com>
2020-08-26 17:40:59 -0400

    open weather api connected to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import './App.css';
+import Weather from './components/weather.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
-import Weather from './components/weather.component';
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+const Api_Key = '429736441cf3572838aa10530929f7cd';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+
+    this.getWeather();
+  }
+
+  getWeather = async (e) => {
+    let city = 'London';
+    let country = 'uk';
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
+    );
+
+    const response = await api_call.json();
+
+    console.log(response);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/weather.component.jsx
+++ src/components/weather.component.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+const minmaxTemp = (min, max) => {
+  return (
+    <h3>
+      <span className="px-4">{min}&deg;</span>
+      <span className="px-4">{max}&deg;</span>
+    </h3>
+  );
+};
+
 const Weather = () => {
   return (
     <div className="container">
@@ -8,6 +17,9 @@ const Weather = () => {
         <h5 className="py-4">
           <i className="wi wi-day-sunny display-1"></i>
         </h5>
+        <h1 className="py-2">25&deg;</h1>
+        {minmaxTemp(19, 25)}
+        <h4 className="py-3">Sunny Day</h4>
       </div>
     </div>
   );
