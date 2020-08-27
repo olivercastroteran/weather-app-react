d676d69d28bec2d7ba45d421f33d61de1e1007cc
olivercastroteran <olivercastroteran@gmail.com>
2020-08-27 09:10:35 -0400

    conditional rendering weather data based on form input

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends React.Component {
       description: '',
       error: false,
     };
-    this.getWeather();
     this.weathericon = {
       Thunderstorm: 'wi-thunderstorm',
       Drizzle: 'wi-sleet',
@@ -67,32 +66,37 @@ class App extends React.Component {
   };
 
   getWeather = async (e) => {
-    let city = 'London';
-    let country = 'uk';
-    const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
-    );
+    e.preventDefault();
+    let city = e.target.elements.city.value;
+    let country = e.target.elements.country.value;
 
-    const response = await api_call.json();
+    if (city && country) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
+      );
 
-    console.log(response);
+      const response = await api_call.json();
 
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      temp_celsius: this.calCelsius(response.main.temp),
-      temp_max: this.calCelsius(response.main.temp_max),
-      temp_min: this.calCelsius(response.main.temp_min),
-      description: response.weather[0].description,
-    });
+      console.log(response);
 
-    this.get_WeatherIcon(this.weathericon, response.weather[0].id);
+      this.setState({
+        city: `${response.name}, ${response.sys.country}`,
+        temp_celsius: this.calCelsius(response.main.temp),
+        temp_max: this.calCelsius(response.main.temp_max),
+        temp_min: this.calCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+      });
+
+      this.get_WeatherIcon(this.weathericon, response.weather[0].id);
+    } else {
+      this.setState({ error: true });
+    }
   };
 
   render() {
     return (
       <div className="App">
-        <Form />
+        <Form loadWeather={this.getWeather} error={this.state.error} />
         <Weather
           city={this.state.city}
           country={this.state.country}
--- src/components/form.component.jsx
+++ src/components/form.component.jsx
@@ -4,7 +4,8 @@ import './form.style.css';
 const Form = (props) => {
   return (
     <div className="container">
-      <form>
+      <div>{props.error ? error() : null}</div>
+      <form onSubmit={props.loadWeather}>
         <div className="row">
           <div className="col-md-3 offset-md-2">
             <input
@@ -32,5 +33,13 @@ const Form = (props) => {
     </div>
   );
 };
+
+function error() {
+  return (
+    <div className="alert alert-danger mx-5" role="alert">
+      Please Enter City and Country
+    </div>
+  );
+}
 
 export default Form;
